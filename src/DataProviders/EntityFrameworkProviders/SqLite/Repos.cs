a40d2dea4b1a9b10436dfae5e34eb1e3ea684147
marchenkoae <marchenkoae@mer.ci.nsu.ru>
2022-11-16 16:04:05 +0700

    Добавили DataProvider.cs test

---
+++ src/DataProviders/EntityFrameworkProviders/SqLite/Repos.cs
@@ -0,0 +1,23 @@
+﻿using DataProviders.Entity_Framework_Providers.Core;
+using DataProviders.EntityFrameworkProviders.Core.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProviders.EntityFrameworkProviders.SqLite
+{
+    public class EFHistorySqLite : EFHistory
+    {
+        protected EFHistorySqLite(DatabaseContext context) : base(context)
+        {
+        }
+    }
+
+    public class EFUsersSqLite : EFUsers
+    {
+        public EFUsersSqLite(DatabaseContext context) : base(context)
+        {
+        }
+    }
+
+}
---
+++ src/DataTests/DataManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Tests
+{
+    [TestClass()]
+    public class DataManagerTests
+    {
+        [TestMethod()]
+        public void DataManagerTest()
+        {
+
+            Assert.Fail();
+        }
+    }
+}
